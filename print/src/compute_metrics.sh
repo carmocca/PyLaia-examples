09327cf2f1a5d7487428ee303a9afebfcd029831
Carlos Mocholí <carlossmocholi@gmail.com>
2020-07-16 21:14:58 +0200

    Fix compute metrics

--- print/src/compute_metrics.sh
+++ print/src/compute_metrics.sh
@@ -10,35 +10,25 @@ SDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
 rm -f decode/decode.out;
 
 for i in char word; do
-  for j in te va; do
+  for j in va te; do
     echo "# ${i}/${j}:" | tee -a decode/decode.out;
 
     # Compute CER/WER using Kaldi's compute-wer-bootci
-    $HOME/software/kaldi/src/bin/compute-wer-bootci --print-args=false --mode=strict \
+    "$HOME"/software/kaldi/src/bin/compute-wer --print-args=false --mode=strict \
       "ark:data/lang/${i}/${j}.gt" \
       "ark:decode/${i}/${j}.hyp" | \
-    awk -F\  '{ print "Kaldi WER:", $3, "[" $8, $9 "] (95% conf)"  }' | \
     tee -a decode/decode.out;
+    echo "" | tee -a decode/decode.out;
 
     # Compute CER/WER using Rostock's TranskribusErrorRate.
-    # The tool does not match by id. remove it:
-    awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}' data/lang/${i}/${j}.gt > tmp.gt
-    awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}' decode/${i}/${j}.hyp > tmp.hyp
-
-    java -cp $HOME/software/TranskribusErrorRate/target/TranskribusErrorRate-2.2.7-jar-with-dependencies.jar \
+    # The tool does not match by id. Needs same order in gt and hyp
+    # Note: CER is not calculated correctly (github.com/Transkribus/TranskribusErrorRate/issues/4)
+    java -cp "$HOME"/software/TranskribusErrorRate/target/TranskribusErrorRate-2.2.7-jar-with-dependencies.jar \
       eu.transkribus.errorrate.HtrErrorTxt \
-      tmp.gt tmp.hyp | \
-    grep ERR | \
-    gawk -F= '{ printf "Transkribus CER: %.3f\n", $2 * 100 }' | \
+      <(awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}' data/lang/${i}/${j}.gt) \
+      <(awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}' decode/${i}/${j}.hyp) \
+      --wer | \
     tee -a decode/decode.out;
-
-    java -cp $HOME/software/TranskribusErrorRate/target/TranskribusErrorRate-2.2.7-jar-with-dependencies.jar \
-      eu.transkribus.errorrate.HtrErrorTxt \
-      tmp.gt tmp.hyp --wer | \
-    grep ERR | \
-    gawk -F= '{ printf "Transkribus WER: %.3f\n\n", $2 * 100 }' | \
-    tee -a decode/decode.out;
+    echo "" | tee -a decode/decode.out;
   done;
 done;
-
-rm -f tmp.gt tmp.hyp;
