f8dfe7373da445ede72ed620f8eadac7c83a5532
Carlos Mocholí <carlossmocholi@gmail.com>
2020-07-20 17:52:56 +0200

    Use a loop

--- print/src/clean_bad_images.sh
+++ print/src/clean_bad_images.sh
@@ -12,19 +12,16 @@ if [ "$check" = true ]; then
 fi
 
 # Remove problematic samples:
+declare -a ids=(
+  "ONB_ibn_19110701_034.r_4_1.tl_3" # 4x3px
+  "ONB_krz_19110701_001.region_1547189437258_60.line_1547189690857_202" # 53x2653
+  "ONB_nfp_18950706_012.r_15_1.line_1548399898797_287" # 33x51
+  "ONB_nfp_19330701_015.TextRegion_1550032000014_3177.line_1550032000280_3185" # 28x28
+  "ONB_nfp_19330701_015.TextRegion_1550032000014_3177.line_1550032000295_3187" # 28x28
+)
 
-# 4x3px "Seite 34. Nr. 148."
-rm -f data/imgs/lines/tr/ONB_ibn_19110701_034.r_4_1.tl_3.png;
-sed -i '/^ONB_ibn_19110701_034\.r_4_1\.tl_3/d' data/lang/word/tr.gt;
-
-# 54x2653px "Beraubte Opfer einer Automobil = Katastrophe ."
-rm -f data/imgs/lines/tr/ONB_krz_19110701_001.region_1547189437258_60.line_1547189690857_202.png
-sed -i '/^ONB_krz_19110701_001\.region_1547189437258_60\.line_1547189690857_202/d' data/lang/word/tr.gt;
-
-# 33x51px "—"
-rm -f data/imgs/lines/tr/ONB_nfp_18950706_012.r_15_1.line_1548399898797_287.png
-sed -i '/^ONB_nfp_18950706_012\.r_15_1\.line_1548399898797_287/d' data/lang/word/tr.gt;
-
-# 28x28px "-"
-rm -f data/imgs/lines/tr/ONB_nfp_19330701_015.TextRegion_1550032000014_3177.line_1550032000280_3185.png;
-sed -i '/^ONB_nfp_19330701_015\.TextRegion_1550032000014_3177\.line_1550032000280_3185/d' data/lang/word/tr.gt;
+for id in "${ids[@]}"; do
+  rm -fv "data/imgs/lines/tr/${id}.*";
+  sed -i "/^${id}/d" data/lang/word/tr.gt;
+  if grep -q "${id}" data/lang/word/tr.gt; then echo "${id} was not correctly removed"; fi
+done
