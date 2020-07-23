5f84bec209e73d45a505b2fccac78a87ffa31379
Carlos Mocholí <carlossmocholi@gmail.com>
2020-07-23 15:42:00 +0200

    Fix compute metrics for tok and no tok

--- print/src/compute_metrics.sh
+++ print/src/compute_metrics.sh
@@ -7,28 +7,25 @@ SDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
     echo "Please, run this script from the experiment top directory!" >&2 && \
     exit 1;
 
-rm -f decode/decode.out;
+# Compute CER/WER using Kaldi's compute-wer-bootci
+echo "# char/va_og:";
+"$HOME"/software/kaldi/src/bin/compute-wer --print-args=false --mode=strict \
+  "ark:data/lang/char/va_og.gt" "ark:decode/char/va.hyp";
+echo -e "\n# char/te_og:";
+"$HOME"/software/kaldi/src/bin/compute-wer --print-args=false --mode=strict \
+  "ark:data/lang/char/te_og.gt" "ark:decode/char/te.hyp";
+# Rostock's tool is not used to calculate CER due to a problem
+# with its tokenizer (github.com/Transkribus/TranskribusErrorRate/issues/4)
 
-for i in char word; do
-  for j in va te; do
-    echo "# ${i}/${j}:" | tee -a decode/decode.out;
+for x in va_og va te_og te; do
+  echo -e "\n# word/${x}:";
+  "$HOME"/software/kaldi/src/bin/compute-wer --print-args=false --mode=strict \
+    "ark:data/lang/word/${x}.gt" "ark:decode/word/${x}.hyp";
 
-    # Compute CER/WER using Kaldi's compute-wer-bootci
-    "$HOME"/software/kaldi/src/bin/compute-wer --print-args=false --mode=strict \
-      "ark:data/lang/${i}/${j}.gt" \
-      "ark:decode/${i}/${j}.hyp" | \
-    tee -a decode/decode.out;
-    echo "" | tee -a decode/decode.out;
-
-    # Compute CER/WER using Rostock's TranskribusErrorRate.
-    # The tool does not match by id. Needs same order in gt and hyp
-    # Note: CER is not calculated correctly (github.com/Transkribus/TranskribusErrorRate/issues/4)
-    java -cp "$HOME"/software/TranskribusErrorRate/target/TranskribusErrorRate-2.2.7-jar-with-dependencies.jar \
-      eu.transkribus.errorrate.HtrErrorTxt \
-      <(cut -d" " -f2- data/lang/${i}/${j}.gt) \
-      <(cut -d" " -f2- decode/${i}/${j}.hyp) \
-      --wer | \
-    tee -a decode/decode.out;
-    echo "" | tee -a decode/decode.out;
-  done;
-done;
+  # Compute CER/WER using Rostock's TranskribusErrorRate.
+  # The tool does not match by id. Needs same order in gt and hyp
+  java -cp "$HOME"/software/TranskribusErrorRate/target/TranskribusErrorRate-2.2.7-jar-with-dependencies.jar \
+    eu.transkribus.errorrate.HtrErrorTxt \
+    <(cut -d" " -f2- "data/lang/word/${x}.gt") \
+    <(cut -d" " -f2- "decode/word/${x}.hyp") --wer;
+done
