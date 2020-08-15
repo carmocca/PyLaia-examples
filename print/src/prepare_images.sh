df47c7220aaa7245a41510850343584619f5feeb
Carlos Mocholí <carlossmocholi@gmail.com>
2020-08-15 17:42:56 +0200

    Fix height using textFeats

--- print/src/prepare_images.sh
+++ print/src/prepare_images.sh
@@ -8,78 +8,24 @@ SDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
 echo "Please, run this script from the experiment top directory!" >&2 && \
 exit 1;
 
-height=128;
-
-cfg="$(mktemp)";
-cat <<EOF > "$cfg"
-TextFeatExtractor: {
-  verbose = false;
-  // Whether to do automatic desloping of the text
-  deslope = false;
-  // Whether to do automatic deslanting of the text
-  deslant = false;
-  // Type of feature to extract, either "dotm" or "raw"
-  type = "raw";
-  // Output features format, either "htk", "ascii" or "img"
-  format = "img";
-  // Whether to do contrast stretching
-  stretch = false;
-  // Window size in pixels for local enhancement
-  enh_win = 30;
-  // Sauvola enhancement parameter
-  enh_prm = 0.1;
-  // 3 independent enhancements, each in a color channel
-  //enh_prm = [ 0.05, 0.2, 0.5 ];
-  // Normalize image heights
-  normheight = 0;
-  normxheight = 0;
-  // Global line vertical moment normalization
-  momentnorm = true;
-  // Whether to compute the features parallelograms
-  fpgram = true;
-  // Whether to compute the features surrounding polygon
-  fcontour = true;
-  fcontour_dilate = 0;
-  // Padding in pixels to add to the left and right
-  padding = 10;
-}
-EOF
-
-# If image height < fix_height, pad with white.
-# If image height > fix_height, scale.
-function fix_image_height () {
-  [ $# -ne 3 ] && \
-  echo "Usage: fix_image_height <fix_height> <input_img> <output_img>" >&2 && \
-  return 1;
-
-  h=$(identify -format '%h' "$2") || return 1;
-  if [ "$h" -lt "$1" ]; then
-    convert -gravity center -extent "x$1" +repage -strip "$2" "$3" || return 1;
-  else
-    convert -resize "x$1" +repage -strip "$2" "$3" || return 1;
-  fi;
-  return 0;
-}
-
-# Clean line images with textFeats
+# No height normalization
 mkdir -p data/imgs/lines/{tr,va,te};
 for set in tr va te; do
   find data/imgs/lines_og/${set} -name "*.png" |
-  xargs $HOME/software/textFeats/build/textFeats \
-    --cfg="$cfg" \
+  xargs textFeats \
+    --cfg=src/textFeats.cfg \
     --outdir=data/imgs/lines/${set} \
     --overwrite=true \
     --threads=$(nproc);
 done
 
-# Resize line images to a fixed height
-mkdir -p data/imgs/lines_h${height}/{tr,va,te};
+# 128px height
+mkdir -p data/imgs/lines_h128/{tr,va,te};
 for set in tr va te; do
-  n=0;
-  for f in $(find data/imgs/lines/${set} -name "*.png"); do
-    ( fix_image_height "${height}" data/imgs/lines{,_h128}/${set}/$(basename ${f}) || exit 1; ) &
-    ((++n));
-    [ "$n" -eq "$(nproc)" ] && { wait; n=1; }
-  done;
-  wait;
-done;
+  find data/imgs/lines_og/${set} -name "*.png" |
+  xargs textFeats \
+    --cfg=src/textFeats_h128.cfg \
+    --outdir=data/imgs/lines_h128/${set} \
+    --overwrite=true \
+    --threads=$(nproc);
+done
